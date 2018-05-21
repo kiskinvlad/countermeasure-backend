7142a071f182150b42816348e379964c0540ea1b
kiskinvlad <kiskinvlad@gmail.com>
2018-05-21 17:16:11 +0300

    protect api routes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var models = require("./models");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('secret', CONFIG.jwt_encryption);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -57,10 +58,8 @@ models.sequelize.sync().then(function() {
     console.log(err, "Something went wrong with the Database Update!")
 
 });
-
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/roles', rolesRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
