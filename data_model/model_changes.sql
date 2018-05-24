779f1d5d9e058527ea85455676fce9f5765b972e
Shannon Lui <shannon.lui@jig.to>
2018-05-24 09:44:23 -0400

    Update data model changes so that it runs

--- data_model/model_changes.sql
+++ data_model/model_changes.sql
@@ -3,12 +3,12 @@
 -- Added by: Shawn Bedasrd on behalf of Jean-Phillippe Tremblay
 
 
-ALTER TABLE CASE ADD COLUMN updated_at DATE (40) NOT NULL DEFAULT NOW() ON UPDATE NOW()after name; 
-ALTER TABLE CASE ADD COLUMN updated_by_name VARCHAR(63) after updated_at; 
-ALTER TABLE CASE ADD COLUMN updated_by_id INT after updated_by_name;
-ALTER TABLE CASE ADD CONSTRAINT `fk_CASE_USER_UPDATEDBY`
+ALTER TABLE `CASE` ADD COLUMN `updated_at` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW() after `name`; 
+ALTER TABLE `CASE` ADD COLUMN `updated_by_name` VARCHAR(63) after `updated_at`; 
+ALTER TABLE `CASE` ADD COLUMN `updated_by_id` int(11) after `updated_by_name`;
+ALTER TABLE `CASE` ADD CONSTRAINT `fk_CASE_USER_UPDATEDBY`
     FOREIGN KEY (`updated_by_id`)
-    REFERENCES `USER` (`user_id`)
+    REFERENCES `USER` (`userid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION;
 
