45052e07e994a75df2737132c71bfd5ec41d4009
Shawn Bedard <sabedard@jig.to>
2018-05-24 09:06:59 -0400

    The data model change log for all to use

---
+++ data_model/model_changes.sql
@@ -0,0 +1,18 @@
+-- MySQL Model changes post initiation 
+-- Thrusday May 24, 2018  
+-- Added by: Shawn Bedasrd on behalf of Jean-Phillippe Tremblay
+
+
+ALTER TABLE CASE ADD COLUMN updated_at DATE (40) NOT NULL DEFAULT NOW() ON UPDATE NOW()after name; 
+ALTER TABLE CASE ADD COLUMN updated_by_name VARCHAR(63) after updated_at; 
+ALTER TABLE CASE ADD COLUMN updated_by_id INT after updated_by_name;
+ALTER TABLE CASE ADD CONSTRAINT `fk_CASE_USER_UPDATEDBY`
+    FOREIGN KEY (`updated_by_id`)
+    REFERENCES `USER` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION;
+
+-- Thrusday May 24, 2018  
+-- Added by: Vlad Kiskin
+
+
