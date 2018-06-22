c84e9ba843f85a585fb6a215d97e676c09e317d3
Shannon Lui <shannon.lui@jig.to>
2018-06-22 12:39:54 -0400

    Refactor OrganizationController to use OrganizationService

--- routes/organizations.js
+++ routes/organizations.js
@@ -8,11 +8,9 @@ const passport = require('passport');
 require('./../middleware/passport')(passport);
 
 router.post('/', passport.authenticate('jwt', {session:false}), OrganizationController.createOrg);
-router.get('/', passport.authenticate('jwt', {session:false}), OrganizationController.getOrg);
 router.get('/statistics', passport.authenticate('jwt', {session:false}), OrganizationController.getOrgStats);
 router.get('/:id', passport.authenticate('jwt', {session:false}), OrganizationController.getOrgByID);
 router.put('/:id', passport.authenticate('jwt', {session:false}), OrganizationController.updateOrg);
-router.delete('/:id', passport.authenticate('jwt', {session:false}), OrganizationController.removeOrg);
 router.get('/:id/permissions', passport.authenticate('jwt', {session:false}), PermissionController.getPermissionsByUserID);
 
 module.exports = router;
